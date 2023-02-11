8b5847b5b19c79ff90c4c3ce46765f7b1b723020
codersharon <124261315+codersharon@users.noreply.github.com>
2023-02-11 09:28:29 +0530

    slice_error_solution-1

--- script.js
+++ script.js
@@ -78,7 +78,8 @@ async function getTechNews() {
 			// showing news
 	let str = ""
 	for(let news of resjson.articles) {
-		news.description = news.description.slice(0, 100) + "..."
+		let d = news.description;
+		news.description = d.slice(0, 100) + "...";
 		if (news.urlToImage == null) {
 			str = str + `<div class="col">
 				<div class="card text-bg-dark">
